dc63358bc1ad3a470126045db7fa41e64bdd66fb
Kentjohn-69 <93919847+Kentjohn-69@users.noreply.github.com>
2024-09-03 19:52:08 +0800

    Add files via upload
    API first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const PORT = 9000
+const express = require('express')
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+const app = express()
+
+const articles = []
+
+app.get('/', (req, res) => {
+    res.json('Welcome to my Climate Change News API fuck me')
+})
+
+app.get('/news', (req, res) =>{
+    axios.get('https://www.theguardian.com/environment/climate-crisis')
+        .then((response) => {
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            $('a:contains("climate")', html).each(function () {
+                const title = $(this).text()
+                 const url = $(this).attr('href')
+                articles.push({
+                    title,
+                    url,
+                })
+            })
+            res.json(articles)
+        }).catch((err) => console.log(err))
+})
+
+app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
